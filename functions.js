243da96ce22064bb2d189b607ac4116ce1ebb11b
kjkrzywosinski <kjkrzywo@svsu.edu>
2025-06-20 22:19:27 -0400

    Update functions.js

--- functions.js
+++ functions.js
@@ -5,7 +5,7 @@ function calculateTotal(quantity, price) {
 
 function outputCartRow(item, total) {
     document.write("<tr>");
-    document.write(`<td><img src="./images/${item.product.filename}" class="painting" alt=""></td>`);
+    document.write(`<td><img src="./art-store/images/${item.product.filename}" class="painting" alt=""></td>`);
     document.write(`<td class="focus">${item.product.title}</td>`); 
     document.write(`<td class="center">${item.quantity}</td>`);
     document.write(`<td class="right">$${item.product.price.toFixed(2)}</td>`);
